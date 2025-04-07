16c4127dac0da971cff0f8ea1f2c585a1be6d332
fragskye <199886693+fragskye@users.noreply.github.com>
2025-04-07 16:53:06 -0700

    Add popup when the player switches

--- prefabs/ui/hud/notification_layer.gd
+++ prefabs/ui/hud/notification_layer.gd
@@ -5,6 +5,8 @@ const SCORE_EVENT_POPUP: PackedScene = preload("res://prefabs/ui/hud/score_event
 @onready var notification_shadow: ColorRect = %NotificationShadow
 @onready var notification_menu: Control = %NotificationMenu
 @onready var score_event_popup_list: VBoxContainer = %ScoreEventPopupList
+@onready var player_name_label: Label = %PlayerNameLabel
+@onready var big_info_animation_player: AnimationPlayer = %BigInfoAnimationPlayer
 
 func _ready() -> void:
 	EventBus.score_event.connect(_on_score_event)
@@ -15,7 +17,11 @@ func _on_players_initialized() -> void:
 	pass
 
 func _on_player_turn_started(player_data: PlayerData) -> void:
-	pass
+	big_info_animation_player.speed_scale = 1.0 if player_data.cpu == null else 0.333
+	if player_data.cpu == null:
+		show_big_info("YOUR TURN!")
+	else:
+		show_big_info("%s'S TURN!" % player_data.name)
 
 func _process(delta: float) -> void:
 	if score_event_popup_list.get_child_count() > 0:
@@ -27,3 +33,7 @@ func _on_score_event(name: String, score: int) -> void:
 	var score_event_popup: ScoreEventPopup = SCORE_EVENT_POPUP.instantiate()
 	score_event_popup_list.add_child(score_event_popup)
 	score_event_popup.set_data(name, score)
+
+func show_big_info(text: String) -> void:
+	player_name_label.text = text
+	big_info_animation_player.play("show_big_info")
